d7aa09bbf66854a0f01f21a3a8b9a44ebb7c1a3d
Elisha Saylon <shoisathot@gmail.com>
2018-11-01 10:29:03 +0800

    Started Battle Instance Layout
    > created a layout for battle instance
    > tooltip function with animation

---
+++ COMS111_ZeroWaste/Assets/Scripts/Scripts Per Scene/Main Menu/MoveController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveController : MonoBehaviour {
+
+    public float walkSpeed;
+
+    private float screenWidth;
+    private SpriteRenderer playerSprite;
+    private Rigidbody2D playerBody;
+
+    private Vector2 playerVelocity;
+
+    public Button button;
+
+    void Start()
+    {
+        // Get sprite of player
+        playerSprite = GetComponent<SpriteRenderer>();
+
+        // Get rigidbody
+        playerBody = GetComponent<Rigidbody2D>();
+
+        // Get with of screen
+        screenWidth = Screen.width;
+    }
+
+
+    void Update()
+    {
+        /*if (Input.touchCount > 0)
+        {
+            // Get first touch
+            Touch touch = Input.GetTouch(0);
+            if (touch.position.x > screenWidth / 2)
+            {
+                MoveCharacter(1.0f);
+            }
+
+            if (touch.position.x < screenWidth / 2)
+            {
+                MoveCharacter(-1.0f);
+            }
+        }*/
+
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            // Get first touch
+            Touch touch = Input.GetTouch(0);
+            if (touch.position.x > screenWidth / 2)
+            {
+                MoveCharacter(1.0f);
+            }
+
+            if (touch.position.x < screenWidth / 2)
+            {
+                MoveCharacter(-1.0f);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        #if UNITY_EDITOR
+        MoveCharacter(Input.GetAxisRaw("Horizontal"));
+        #endif
+    }
+
+    public void MoveCharacter(float horizontalInput)
+    {
+        // playerSprite.flipX = !playerSprite.flipX;
+        playerBody.AddForce(new Vector2(horizontalInput * walkSpeed * Time.deltaTime, 0));
+        /*Vector2 moveInput = new Vector2(transform.position.x - 1, transform.position.y);
+        playerVelocity = moveInput.normalized * walkSpeed;
+        transform.position = playerVelocity * Time.fixedDeltaTime;
+        Debug.Log("Left: " + playerVelocity);*/
+    }
+
+    public void MoveRight()
+    {
+        playerSprite.flipX = true;
+        Vector2 moveInput = new Vector2(transform.position.x + 1, transform.position.y);
+        playerVelocity = moveInput.normalized * walkSpeed;
+        transform.position = playerVelocity * Time.fixedDeltaTime;
+        Debug.Log("Right: " + playerVelocity);
+    }
+
+}
---
+++ COMS111_ZeroWaste/Assets/Scripts/Scripts Per Scene/Main Menu/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public Transform[] backgrounds;
+    public float smoothing = 1.5f;
+    private Transform parallaxCam;
+    private Vector3 previousCameraPos;
+
+    private float[] parallaxScales;
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
